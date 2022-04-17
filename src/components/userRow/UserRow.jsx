ee3bbe74c30100e1055a75b6646f57698729cc57
ajayzeni <ajay@zeni.ai>
2022-04-17 09:55:27 +0530

    implement actions

--- src/components/userRow/UserRow.jsx
+++ src/components/userRow/UserRow.jsx
@@ -1,9 +1,35 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faSave, faTrashCan, faX } from '@fortawesome/free-solid-svg-icons'
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEdit,
+  faDownload,
+  faTrashCan,
+  faX,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "./UserRow.css";
-const UserRow = ({ memberData, onChange }) => {
-  const { id, name, email, role, isSelected } = memberData;
+const UserRow = ({ userData, onSave, onDelete, onChange }) => {
+  const [isEditOn, setIsEditOn] = useState(false);
+  const [formFileds, setFormFileds] = useState({ ...userData });
+  const { id, name, email, role, isSelected } = formFileds;
+
+  const editHandler = (event) => {
+    const { name, value } = event.target;
+    setFormFileds({ ...formFileds, [name]: value });
+  };
+  const cancelHandler = () => {
+    setIsEditOn(false);
+    setFormFileds({ ...userData });
+  };
+  const saveHandler = () => {
+    setIsEditOn(false);
+    onSave(formFileds);
+  };
+  const deleteHandler = () => {
+    setIsEditOn(false);
+    onDelete(id);
+  };
+
   return (
     <tr>
       <td>
@@ -13,16 +39,70 @@ const UserRow = ({ memberData, onChange }) => {
           checked={isSelected}
         />
       </td>
-      <td>{name}</td>
-      <td>{email}</td>
       <td>
-        <div className="role">{role}</div>
+        {!isEditOn ? (
+          name
+        ) : (
+          <input
+            className="inputLayout"
+            name="name"
+            value={name}
+            onChange={editHandler}
+          />
+        )}
       </td>
       <td>
-        <span><FontAwesomeIcon icon={faEdit} /></span>
-        <span><FontAwesomeIcon icon={faTrashCan} /></span>
-        <span><FontAwesomeIcon icon={faSave} /></span>
-        <span><FontAwesomeIcon icon={faX} /></span>
+        {!isEditOn ? (
+          email
+        ) : (
+          <input
+            className="inputLayout"
+            name="email"
+            value={email}
+            onChange={editHandler}
+          />
+        )}
+      </td>
+      <td>
+        <div className="role">
+          {!isEditOn ? (
+            role
+          ) : (
+            <select
+              className="inputLayout"
+              name="role"
+              value={role}
+              onChange={editHandler}
+            >
+              <option value={"member"}>Member</option>
+              <option value={"admin"}>Admin</option>
+            </select>
+          )}
+        </div>
+      </td>
+      <td>
+        {!isEditOn ? (
+          <div className="actions-container">
+            <span>
+              <FontAwesomeIcon
+                icon={faEdit}
+                onClick={() => setIsEditOn(true)}
+              />
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faTrashCan} onClick={deleteHandler} />
+            </span>
+          </div>
+        ) : (
+          <div className="actions-container">
+            <span>
+              <FontAwesomeIcon icon={faDownload} onClick={saveHandler} />
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faX} onClick={cancelHandler} />
+            </span>
+          </div>
+        )}
       </td>
     </tr>
   );
